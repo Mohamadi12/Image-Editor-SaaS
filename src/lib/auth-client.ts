7342b67463f73682cbb79009ac700d46c57bd9a1
Nana Mohamadi <adilnana78@gmail.com>
2025-09-26 07:29:50 +0000

    Polar config and Webhooks extraction

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import { env } from "process";
+import { polarClient } from "@polar-sh/better-auth";
 
 export const authClient = createAuthClient({
   baseURL: env.BETTER_AUTH_URL,
+  plugins: [polarClient()],
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
+import { Polar } from "@polar-sh/sdk";
+import { env } from "~/env";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+
+const polarClient = new Polar({
+  accessToken: env.POLAR_ACCESS_TOKEN,
+  server: "sandbox",
+});
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -11,4 +24,42 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "123-456-789", // ID of Product from Polar Dashboard
+              slug: "pro", // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+            },
+          ],
+          successUrl: "/success?checkout_id={CHECKOUT_ID}",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+        checkout({
+          products: [
+            {
+              productId: "dcfa3c93-0a88-4e8a-b16e-37b6226920aa",
+              slug: "small",
+            },
+            {
+              productId: "c2d5855c-3164-48d0-95e1-422dd6a1758f",
+              slug: "medium",
+            },
+            {
+              productId: "63836be1-33cc-4c81-9e67-3f95848331d8",
+              slug: "large",
+            },
+          ],
+          successUrl: "/dashboard",
+          authenticatedUsersOnly: true,
+        }),
+      ],
+    }),
+  ],
 });
